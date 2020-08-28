e2103b55ec61f26e0e1b7cc74ffe89bb061f0f11
Sirui911 <siruipan@usc.edu>
2020-08-28 11:58:37 -0700

    Add billing and client side App

--- index.js
+++ index.js
@@ -33,13 +33,15 @@ app.use(passport.session());
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
