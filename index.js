eced8e3dd8775d9b59a36d13318461843f2e6755
Sirui911 <siruipan@usc.edu>
2020-08-28 11:41:32 -0700

    Add billing and client side App

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //以下代码只在production的时候运行
-if (process.env.NODE_ENV === 'production'){
-  //express will server up production assets, like our main.js or main.css
-  //虽然express不能处理，但是可以找到具体文件
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
 
-  //express will server up the index.html file if it doesn't recognize the file
-  //对应无法处理，所以返回index.html
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
-  app.get('*', (req, res) =>{
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
